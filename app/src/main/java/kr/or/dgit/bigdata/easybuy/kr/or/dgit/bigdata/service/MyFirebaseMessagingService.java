0b5c123bd3f142a84ce1ea8febd020e918171f9f
Mosquito(H) <skykim10908@naver.com>
2018-04-28 15:42:56 +0900

    fcm update

--- app/src/main/java/kr/or/dgit/bigdata/easybuy/kr/or/dgit/bigdata/service/MyFirebaseMessagingService.java
+++ app/src/main/java/kr/or/dgit/bigdata/easybuy/kr/or/dgit/bigdata/service/MyFirebaseMessagingService.java
@@ -1,9 +1,23 @@
 package kr.or.dgit.bigdata.easybuy.kr.or.dgit.bigdata.service;
 
+
+
+
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import kr.or.dgit.bigdata.easybuy.R;
+import kr.or.dgit.bigdata.easybuy.StartActivity;
 
 /**
  * Created by KDH on 2018-04-27.
@@ -14,14 +28,38 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        if (remoteMessage.getNotification() != null) {
-            Log.e(TAG, "Title: " + remoteMessage.getNotification().getTitle());
-            Log.e(TAG, "Body: " + remoteMessage.getNotification().getBody());
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = null;
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            String channel_Id = "EasyBuy Channel";
+            String channel_Name = "EasyBuy";
+            String channel_Description = "EasyBuy Description";
+
+            NotificationChannel channel = new NotificationChannel(channel_Id, channel_Name, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(channel_Description);
+
+            manager.createNotificationChannel(channel);
+            builder = new NotificationCompat.Builder(this, channel_Id);
+        }else{
+            builder = new NotificationCompat.Builder(this);
         }
 
-        if (remoteMessage.getData().size() > 0) {
-            Log.e(TAG, "Data: " + remoteMessage.getData());
-        }
+        builder.setSmallIcon(R.drawable.message_logo);
+        builder.setWhen(System.currentTimeMillis());
+        builder.setContentTitle(remoteMessage.getNotification().getTitle());
+        builder.setContentText(remoteMessage.getNotification().getBody());
+        builder.setDefaults(Notification.DEFAULT_VIBRATE| Notification.DEFAULT_SOUND);
+        builder.setColor(getResources().getColor(R.color.whiteTextColor));
+
+        Intent intent = new Intent(this, StartActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 10, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setContentIntent(pendingIntent);
+
+        manager.notify(1004, builder.build());
+
 
     }
 }
